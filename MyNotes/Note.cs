966c6250b6a0035db2f77f1723822aa037f5f3d4
GBP <greenblackpanther2@gmail.com>
2019-01-21 15:58:27 +0100

    Added abstract class Note

---
+++ MyNotes/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes
+{
+    abstract class Note
+    {
+        private int _id;
+        public int Id
+        {
+            get { return this._id; }
+        }
+
+        private string _text;
+        public string Text
+        {
+            get { return this._text; }
+            set { this._text = value; }
+        }
+
+        public Note()
+        {
+            this._id = -1;
+            this._text = "";
+        }        
+
+    }
+}
